fdefcf3ee57292228284c80fb24a24d68011556d
Jack Garratt <jack.garratt@icfnext.com>
2021-11-14 20:07:58 -0500

    Make axios call for madlib using useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return(
+
+  useEffect(() => {
+    const fetchMadlib = async () => {
+      const { data } = await axios.get('http://madlibz.herokuapp.com/api/random', {
+        params: {
+          minlength: 2,
+          maxlength: 25,
+        }
+      });
+      console.log(data);
+    }
+
+    fetchMadlib();
+  }, []);
+
+  return (
     <h1>Madlibz App</h1>
   );
 };
